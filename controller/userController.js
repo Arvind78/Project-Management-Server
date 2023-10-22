35b2d8a9ad8f8f329b63c0a8f08d1269afd75c34
Arvind <av894883@gmail.com>
2023-10-21 19:30:54 -0700

    changeDone

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcryptjs');
 const UserModel = require("../model/userModel.js");
 const jwt = require('jsonwebtoken');
-const error = require('../error/error.js');
 
 /**
  * Controller function for user signup
@@ -15,7 +14,7 @@ const userSignup = async (req, res, next) => {
         // Check if the user already exists
         const existingUser = await UserModel.findOne({ email });
         if (existingUser) {
-            return res.status(409).json({ success: false, message: "User account already exists!" });
+            return res.status(409).json({ success: false, message: "User account already exists !" });
         }
 
         // Hash the password before saving it in the database
@@ -28,7 +27,7 @@ const userSignup = async (req, res, next) => {
         });
 
         // Return success response
-        res.status(201).json({ success: true, message: "User account created successfully!" });
+        res.status(201).json({ success: true, message: "User account created successfully." });
     } catch (error) {
         // Handle errors and pass them to the error handling middleware
         next(error);
@@ -47,20 +46,20 @@ const userLogin = async (req, res, next) => {
         // Find the user by email
         const user = await UserModel.findOne({ email });
         if (!user) {
-            return res.status(404).json({ success: false, message: "Invalid Credentials" });
+            return res.status(404).json({ success: false, message: "Invalid Credentials !" });
         }
 
         // Compare the provided password with the stored hashed password
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
-            return res.status(401).json({ success: false, message: "Invalid Email or Password" });
+            return res.status(401).json({ success: false, message: "Invalid Email or Password." });
         }
 
         // Generate a login token based on user login status
         const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
 
         // Return success response with token
-        res.status(200).json({ success: true, message: "Valid User", token });
+        res.status(200).json({ success: true, message: "Valid User .", token });
     } catch (error) {
         // Handle errors and pass them to the error handling middleware
         next(error);
@@ -81,16 +80,16 @@ const forgotPassword = async (req, res, next) => {
             // Logic for verification email check
             const user = await UserModel.findOne({ email });
             if (!user) {
-                return next(error(404,'Email is ragisterd'));
+                return res.status(404).json({ success: false, message: "Email is ragistered !" });
             } else {
-                return res.status(201).json({ userId: user._id, message: "User verification successful!" });
+                return res.status(201).json({ userId: user._id, message: "User verification successful." });
             }
         } else {
             // Hash the new password before updating in the database
             const hashedPassword = await bcrypt.hash(newPassword, 10);
             // Update user password in the database using userId
             const updatedUser = await UserModel.findByIdAndUpdate(userId, { $set: { password: hashedPassword } }, { new: true });
-            return res.status(201).json({ message: "User password reset successfully!" });
+            return res.status(201).json({ message: "User password reset successfully." });
         }
     } catch (error) {
         // Handle errors and pass them to the error handling middleware
